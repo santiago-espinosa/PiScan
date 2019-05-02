b0a356854b9f29eb4cedf26a9a8ecff4d395c4dd
IMTSSE <santiago.espinosa14@pm.me>
2019-05-02 14:37:48 +0800

    Fixed absolute paths

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 #/!/bin/bash
 
-A=$(wc ../data/add_count.txt -l | cut -d " " -f 1)
+A=$(wc data/add_count.txt -l | cut -d " " -f 1)
 if [ 90 -lt $A ]
 then
-        tail -n 90 ../data/add_count.txt > ../data/add_count1.txt
-        mv ../data/add_count1.txt ../data/add_count.txt
+        tail -n 90 data/add_count.txt > data/add_count_cleanup_cache.txt
+        rm data/add_count.txt
+        mv data/add_cleanup_cache.txt data/add_count.txt
+        rm data/add_cleanup_cache.txt
 fi
