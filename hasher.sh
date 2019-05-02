905460e058985968b4bb8d623afd27f61fa811c9
IMTSSE <santiago.espinosa14@pm.me>
2019-05-02 14:38:09 +0800

    Added parameter check

--- hasher.sh
+++ hasher.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#check if there are any arguments and exit otherwise
+[ "$#" -ne 1 ] && exit
 while IFS='' read -r line || [[ -n "$line" ]]; do
     md5sum $line >> data/prev_add_hashed.txt
 done < data/$i/prev_add.txt
