f2ba0f0d1dd3f587c01a5af09b1c27257c5702ed
IMTSSE <santiago.espinosa14@pm.me>
2019-05-02 14:37:26 +0800

    Added directory check

--- startup.sh
+++ startup.sh
@@ -8,17 +8,23 @@ while :
 do
     for i in $A; do
 
+        #check if dir exists
+        if [ ! -d "data/$i" ]; then
+            mkdir data/$1;
+            mkdir temp/$1;
+        fi
+
         #Set given wireless interface to monitor mode if not the case
         D=$(iw dev  | grep "managed")
         if [[ "$D" == *"managed"* ]]; then
-            sudo ip link set $i down
-            sudo iw $i set monitor control
-            sudo ip link set $i up
+            sudo ip link set $i down;
+            sudo iw $i set monitor control;
+            sudo ip link set $i up;
         fi
 
         #run scripts in the background and continue execution
-        nohup ./startup.sh $i &>/dev/null &
-        nohup ./hasher.sh $i &>/dev/null &
+        nohup ./startup.sh $i &>/dev/null &;
+        nohup ./hasher.sh $i &>/dev/null &;
 
         sleep 15;
     done
