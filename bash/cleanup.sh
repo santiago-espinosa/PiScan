04612956aa6108962c017294e326895e7e6d8f7f
santiago <santiago.espinosa14@pm.me>
2019-05-03 11:34:53 +0800

    Changed to delete only when log > 180

--- bash/cleanup.sh
+++ bash/cleanup.sh
@@ -1,9 +1,9 @@
 #/!/bin/bash
 
 A=$(wc data/add_count.txt -l | cut -d " " -f 1)
-if [ 90 -lt $A ]
+if [ 180 -lt $A ]
 then
-        tail -n 90 data/add_count.txt > data/add_count_cleanup_cache.txt
+        tail -n 180 data/add_count.txt > data/add_count_cleanup_cache.txt
         rm data/add_count.txt
         mv data/add_cleanup_cache.txt data/add_count.txt
         rm data/add_cleanup_cache.txt
