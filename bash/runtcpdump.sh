85e92465776b2de4eebf055f5c3b2f49698061af
santiago <santiago.espinosa14@pm.me>
2019-05-28 22:36:29 +0800

    fix bugs

--- bash/runtcpdump.sh
+++ bash/runtcpdump.sh
@@ -4,6 +4,6 @@ FOLDER=$(ls -d /home/pi/piScan/logs/*/ | tail -1)
 TIME=$(date +%s%N | cut -b1-13)
 while :
 do
-    sudo nohup tcpdump -i wlan0 -G 15 -e -s 0 -l type mgt subtype probe-req or type data subtype null or type mgt subtype assoc-req or type mgt subtype reassoc-req | grep -P --line-buffered -o '(?<=SA:)(([a-f]|[0-9]){2}:){5}([a-f]|[0-9]){2}' | awk -Winteractive -v '{md5}' | sort -u > $FOLDER/$TIME.macmd5
+    sudo nohup tcpdump -i wlan0 -G 15 -e -s 0 -l type mgt subtype probe-req or type data subtype null or type mgt subtype assoc-req or type mgt subtype reassoc-req | grep -P --line-buffered -o '(?<=SA:)(([a-f]|[0-9]){2}:){5}([a-f]|[0-9]){2}' | > $FOLDER/$TIME
     sleep 5.5
 done
