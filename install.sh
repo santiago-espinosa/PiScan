1e1da738fe2ea32cd4b9a02effa9a55e122471c2
Unknown <santiago.espinosa14@pm.me>
2019-06-12 10:53:20 +0800

    Updated install.sh to be more versatile

--- install.sh
+++ install.sh
@@ -1,45 +1,48 @@
 #/!bin/bash
-ISTALLDIR="/usr/bin"
+INSTALLDIR="/usr/bin"
 CDIR=$(pwd)
-
-if [ ! -w /etc/init.d ]; then echo "Make must be run as root in order to install as system services."; exit -1; fi
-
+if [ ! -w /etc/init.d ]; then
+    echo "Script must be run as root in order to install programs as system services."
+    exit -1
+fi
 echo "Updating system"
-sudo apt-get update -qq -y >> /dev/null
-sudo apt-get upgrade -qq -y >> /dev/null
+sudo ./spinner.sh apt-get update -qq -y
+echo "  -update done"
+sudo ./spinner.sh apt-get upgrade -qq -y
+echo "  -upgrade done"
 
 echo "Making sure scripts are executable"
-sudo chmod 755 $CDIR/bash/*.sh
+sudo chmod u+x $CDIR/bash/*
 
-#install dependent programs
-echo "intalling tcpdump"
-sudo apt-get install tcpdump -qq -y >> /dev/null
+echo "intalling dependent programs"
+sudo ./spinner.sh apt-get install tcpdump -qq -y
+echo "  -tcpdump install done"
 
-
-#moving files, creating folders and setting API key (mac address)
 echo "Installing files and systemd service"
-sudo mkdir $ISTALLDIR/PiScan
-sudo mkdir $ISTALLDIR/PiScan/info /tmp/PiScan /tmp/PiScan/logs $ISTALLDIR/PiScan/scripts
+sudo mkdir -p $INSTALLDIR/PiScan
+sudo mkdir -p $INSTALLDIR/PiScan/info $INSTALLDIR/PiScan/data /tmp/PiScan /tmp/PiScan/logs $INSTALLDIR/PiScan/bash
+echo "  -create folders done"
+sudo cp $CDIR/bash/* $INSTALLDIR/PiScan/bash/
+sudo cp $CDIR/services/* /lib/systemd/system/
+echo "  -copy scripts done"
 
-sudo cp $CDIR/scripts/*.sh $ISTALLDIR/PiScan/scripts/
-
-#HOSTNAME=$(ifconfig -a | awk '/^[a-z]/ { iface=$1; mac=$NF; next } /inet addr:/ { print iface, mac }')
-#touch $ISTALLDIR/PiScan/info/API.key
-#echo "$hostname" > $ISTALLDIR/info/API.key
-#g++ $CDIR/c++/hasher.cpp -o $ISTALLDIR/PiScan/bin/exec
-
-
-#Installing and starting systemd service
-sudo cp $CDIR/services/*.service /lib/systemd/system/
-
-#start scripts
-sudo systemctl daemon-reload
-sudo systemctl start tcpdumpsetup.service
-sudo systemctl start tcpdumpchanhop.service
-sudo systemctl start tcpdump.service
+echo "Starting services"
+sudo systemctl daemon-reload >> /dev/null
+sudo systemctl start tcpdumpsetup.service >> /dev/null
+echo "  -setup.sh service running"
+sudo systemctl start tcpdumpchanhop.service >> /dev/null
+echo "  -chanhop.sh service running"
+sudo systemctl start tcpdump.service >> /dev/null
+echo " -tcpdump.sh service running"
 
 echo "Install finished"
-read -p "Remove pre-installation files? (y/n)" answer
-if [ "$answer" == 'y']; then
-    rm -r $CDIR/
+
+echo "Remove pre-installation files? (y/n)"
+read answer
+yes="y"
+if [ "$answer" == "$yes" ]; then
+    rm -r ./
+    exit 1
+else
+    exit 1
 fi
