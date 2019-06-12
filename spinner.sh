ec2a31d1032d75cf4e2c2da8a268b043aa189b09
Unknown <santiago.espinosa14@pm.me>
2019-06-12 10:52:21 +0800

    Added spinner.sh to show fancy spinning while installing

---
+++ spinner.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+show_spinner()
+{
+  local -r pid="${1}"
+  local -r delay='0.4'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf " \r"
+  done
+  printf " \r"
+}
+
+("sudo $@") &
+show_spinner "$!"
