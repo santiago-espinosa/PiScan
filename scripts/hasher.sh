19e6b1f5fc8a9804a9a59f8337a62bb40aa543c4
Santiago <santiago.espinosa14@pm.me>
2020-02-12 14:51:46 +0800

    Changed working directory
    Working directory is now $(pwd)/piscan instead of ~/piscan

--- scripts/hasher.sh
+++ scripts/hasher.sh
@@ -2,7 +2,7 @@
 date_today=$(date +%F)
 
 while read p; do
-  touch ~/piscan/output/$1.hash
-  echo "$date_today $p" | md5sum | cut -f1 -d' ' > ~/piscan/output/$1.hash
-  echo -e "$p\t $1\n" >> ~/piscan/output/$1.hash
-done < ~/piscan/mac_addresses/$1
+  touch $(pwd)/piscan/output/$1.hash
+  echo "$date_today $p" | md5sum | cut -f1 -d' ' > $(pwd)/piscan/output/$1.hash
+  echo -e "$p\t $1\n" >> $(pwd)/piscan/output/$1.hash
+done < $(pwd)/piscan/mac_addresses/$1
--- scripts/runer.sh
+++ scripts/runer.sh
@@ -2,7 +2,7 @@
 tcp_dump=("sudo nohup tcpdump -i wlan0 -G 15 -e -s 0 -l type mgt subtype probe-req or type data subtype null or type mgt subtype assoc-req or type mgt subtype reassoc-req | grep -P --line-buffered -o '(?<=SA:)(([a-f]|[0-9]){2}:){5}([a-f]|[0-9]){2}'")
 
 #check if setup has been run already
-if [ ! -d "~/piscan" ]; then
+if [ ! -d "$(pwd)/piscan" ]; then
     echo "Folder not found. Running setup."
     bash setup.sh
 fi
@@ -10,7 +10,7 @@ fi
 for i in {1..3}
 do
     this_round=$(date +%s%N | cut -b1-13)
-    $tcp_dump > ~/piscan/mac_addresses/$this_round
+    $tcp_dump > $(pwd)/piscan/mac_addresses/$this_round
     nohup bash hasher.sh $this_round
     sleep 5
 done
